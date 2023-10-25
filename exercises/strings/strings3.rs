04931f176971d39e4ae5d0de9e7a795a140cfc6d
libchaos <libchaos01@163.com>
2023-10-25 17:25:37 +0800

    39done

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -7,12 +7,12 @@
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    input.to_string().push_str(" world!")
 }
 
 fn replace_me(input: &str) -> String {
