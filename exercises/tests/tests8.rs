d55c1b4a79971342f983522edd220e3c40c60582
libchaos <libchaos01@163.com>
2023-11-03 09:50:02 +0800

    100

--- exercises/tests/tests8.rs
+++ exercises/tests/tests8.rs
@@ -9,13 +9,7 @@
 
 
 
-fn main() {
-    let timestamp = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
-
-    // std::env::set_var("TEST_FOO", timestamp.to_string());
-    println!("cargo:rustc-env=TEST_FOO={}", timestamp.to_string());
-    println!("cargo:rustc-cfg=feature=\"pass\"");
-}
+fn main() {}
 
 #[cfg(test)]
 mod tests {
